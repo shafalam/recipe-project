7364a608334a6f9f19f72c25b204417cf008edcd
shafalam <38904470+shafalam@users.noreply.github.com>
2019-04-10 16:06:41 +0300

    Completing assignments

---
+++ src/main/java/com/shafiul/alam/recipeproject/model/Ingredient.java
@@ -0,0 +1,52 @@
+package com.shafiul.alam.recipeproject.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    @ManyToOne
+    private Recipe recipe;
+
+    @OneToOne
+    private UnitOfMeasure uom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
+
+
+}
